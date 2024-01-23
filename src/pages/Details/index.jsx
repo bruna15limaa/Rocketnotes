3ec293c10386a3c20de8009155709d4a4463f638
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-01-23 17:30:53 -0300

    Tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container, Links } from './styles';
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
 
 
 export function Details() {
@@ -16,6 +17,14 @@ export function Details() {
             <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
             
         </Links>
+
+        <Section title="Marcadores">
+          
+          <Tag title="express"/>
+          <Tag title="nodejs"/>
+            
+        </Section>
+
          <Button title="Voltar"/>
             
         </Container>
