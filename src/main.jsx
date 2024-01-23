098a461474cb3d6a8531a217022dc593bfc8374b
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-01-23 13:49:36 -0300

    theme provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Details } from './pages/Details.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import { Details } from './pages/Details';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme} >
     <Details />
-  </React.StrictMode>,
+    </ThemeProvider>
+  </React.StrictMode>
 )
