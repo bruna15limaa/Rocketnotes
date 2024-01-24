1f3a84e1cc1e22c8689fd2078a5c44444447ef0f
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-01-24 17:37:51 -0300

    routes

---
+++ src/Routes/app.routes.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import { New } from '../pages/New';
+import { Home } from '../pages/Home';
+import { Profile } from '../pages/Profile';
+import { Details } from '../pages/Details';
+
+export function Routes() {
+    return(
+        <Routes>
+        <Route path='/'element={<Home/>}/>
+        <Route path='/new'element={<New/>}/>
+        <Route path='/profile'element={<Profile/>}/>
+        <Route path='/details'element={<Details/>}/>
+        </Routes>
+    )
+}
