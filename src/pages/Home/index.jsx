119a85a9550c99fcccd57fa8e5341e08a24773b8
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-01-24 13:02:13 -0300

    Notes :tada:

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,42 +1,48 @@
-import { FiPlus, FiSearch } from 'react-icons/fi'
-import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
-import { Note } from '../../components/Note';
-import { Header } from '../../components/Header';
-import { Input } from '../../components/Input';
-import { Section } from '../../components/Section';
+import { FiPlus } from 'react-icons/fi'
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
-import { ButtonText } from '../../components/ButtonText';
+import { Note } from '../../components/Note'
+import { Input } from '../../components/Input'
+import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Home() {
-    return (
-        <Container>
-            <Brand>
-              <h1>Rocketnotes</h1>
-            </Brand>
-            <Header/>
-            
-            <Menu>
-            <li><ButtonText title="Todos" isactive/></li>
-            <li><ButtonText title="React"/></li>
-            <li><ButtonText title="Nodejs"/></li>
+  return (
+    <Container>
+      <Brand>
+        <h1>Rocketnotes</h1>
+      </Brand>
 
-            </Menu>
+      <Header />
 
-            <Search>
-              <Input placeholder="Pesquisar pelo título" />
-            </Search>
+      <Menu>
+        <li><ButtonText title="Todos" $isactive /></li>
+        <li><ButtonText title="React" /></li>
+        <li><ButtonText title="Nodejs" /></li>
+      </Menu>
 
-            <Content>
-              <Section title="Minhas notas">
-                
-              </Section>
-            </Content>
+      <Search>
+        <Input placeholder="Pesquisar pelo título" />
+      </Search>
 
-            <NewNote>
-              <FiPlus />
-              Criar nota
-            </NewNote>
+      <Content>
+        <Section title="Minhas notas">
+          <Note data={{
+            title: 'React',
+            tags: [
+              { id: '1', name: 'react' },
+              { id: '2', name: 'rocketseat' }
+            ]
+          }}
+          />
+        </Section>
+      </Content>
 
-        </Container>
-    );
+      <NewNote>
+        <FiPlus />
+        Criar nota
+      </NewNote>
+    </Container>
+  )
 }
