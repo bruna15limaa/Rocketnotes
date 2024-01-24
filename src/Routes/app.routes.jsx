40f3fe348452b46109015886aac3d1f036b67819
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-01-24 17:46:11 -0300

    Routes

--- src/Routes/app.routes.jsx
+++ src/Routes/app.routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import { New } from '../pages/New';
 import { Home } from '../pages/Home';
