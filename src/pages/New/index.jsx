521cd45e9e4f3a5c5620e3dc65ac6abd8b659f3b
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-01-24 16:00:55 -0300

    Textarea :tada:

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,5 +1,7 @@
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
+
 
 import { Container, Form  } from'./styles';
 
@@ -14,6 +16,7 @@ export function New(){
                             <a href="">voltar</a>
                     </header>
                     <Input placeholder="Título"/>
+                    <Textarea placeholder="Observações"/>
                 </Form>
             </main>
         </Container>
